6061c60a07af344fc6ee198c98c4a180469b55fa
windjay <windjay@daum.net>
2019-01-25 01:41:05 +0900

    스프링 데이터 입문중...

---
+++ src/main/java/com/windjay/springboot/boottest/H2Runner.java
@@ -0,0 +1,37 @@
+package com.windjay.springboot.boottest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class H2Runner implements ApplicationRunner {
+
+  @Autowired
+  DataSource dataSource;
+
+  @Autowired
+  JdbcTemplate jdbcTemplate;
+
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+    try (Connection connection = dataSource.getConnection();) {
+
+      System.out.println(connection.getMetaData().getURL());
+      System.out.println(connection.getMetaData().getUserName());
+
+      Statement stmt = connection.createStatement();
+      String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY(ID))";
+      stmt.executeUpdate(sql);
+
+      jdbcTemplate.execute("INSERT INTO USER VALUES(1, 'windjay')");
+
+    }
+  }
+}

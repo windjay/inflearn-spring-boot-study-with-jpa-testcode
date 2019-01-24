4c83dd4450a7d5f6b2bdde5822744d33ac34017b
windjay <windjay@daum.net>
2019-01-24 21:23:54 +0900

    테스트코드 작성 외 mvc 테스트 바인딩도 쉽고 좋구만

---
+++ src/main/java/com/windjay/springboot/boottest/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.windjay.springboot.boottest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+//아래의 설정을 하면 안된다......
+//@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+
+
+}
---
+++ src/test/java/com/windjay/springboot/boottest/user/UserControllerTest.java
@@ -0,0 +1,44 @@
+package com.windjay.springboot.boottest.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  public void hello() throws Exception {
+    mockMvc.perform(get("/hello"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("hello"));
+  }
+
+  @Test
+  public void createUser_JSON() throws Exception{
+    String userJson ="{\"username\" : \"windjay\", \"password\": \"123\"}";
+    mockMvc.perform(MockMvcRequestBuilders.post("/users/create")
+            .contentType(MediaType.APPLICATION_JSON_UTF8)
+            .accept(MediaType.APPLICATION_JSON_UTF8)
+            .content(userJson))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.username", is(equalTo("windjay"))))
+            .andExpect(jsonPath("$.password", is(equalTo("123"))));
+
+  }
+
+}

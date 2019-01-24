b271877c3e644629da6254de1d8d89b5dfbda17f
windjay <windjay@daum.net>
2019-01-24 17:58:51 +0900

    spring boot study

---
+++ src/main/java/com/windjay/springboot/boottest/BootTestApplication.java
@@ -0,0 +1,14 @@
+package com.windjay.springboot.boottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootTestApplication.class, args);
+  }
+
+}
+
---
+++ src/main/java/com/windjay/springboot/boottest/sample/SampleService.java
@@ -0,0 +1,11 @@
+package com.windjay.springboot.boottest.sample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleService {
+
+  public String getName() {
+    return "windjay";
+  }
+}
